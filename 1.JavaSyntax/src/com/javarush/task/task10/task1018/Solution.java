93e33381eb1d94474cf3fce34c61c88e4190fb46
Alexey Vedensky <avedensky@gmail.com>
2017-03-23 01:40:39 +0300

    1018 Resolved

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1018/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task10.task1018;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Поправочки нужны
+
+Задача: Программа демонстрирует работу HashMap: вводит с клавиатуры набор пар (номер и строку),
+помещает их в HashMap и выводит на экран содержимое HashMap.
+*/
+
+public class Solution {
+    HashMap<Integer, String> map;
+    static Integer index;
+    static String name;
+
+    public Solution() {
+        this.map = new HashMap<Integer, String>();
+        //map.put(index, name);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Solution solution = new Solution();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 10; i++) {
+            int index = Integer.parseInt(reader.readLine());
+            String name = reader.readLine();
+            solution.map.put(index, name);
+        }
+
+        for (Map.Entry<Integer, String> pair : solution.map.entrySet()) {
+            index = pair.getKey();
+            name = pair.getValue();
+            System.out.println("Id=" + index + " Name=" + name);
+        }
+    }
+}
