7d6c1d8f33449348eb2f6e0d1568c8843feff190
Alexey Vedensky <avedensky@gmail.com>
2017-03-24 19:10:23 +0300

    task 2015 has done

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2015/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task20.task2015;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/* 
+Переопределение сериализации
+
+Сделайте так, чтобы после десериализации нить runner продолжила работать.
+Ключевые слова объекта runner менять нельзя.
+
+Hint/Подсказка:
+Конструктор не вызывается при десериализации, только инициализируются все поля.
+*/
+public class Solution implements Serializable, Runnable{
+    transient private Thread runner;
+    private int speed;
+
+    public Solution(int speed) {
+        this.speed = speed;
+        runner = new Thread(this);
+        runner.start();
+    }
+
+    public void run() {
+        // do something here, does not matter
+    }
+
+    /**
+     Переопределяем сериализацию.
+     Для этого необходимо объявить методы:
+     private void writeObject(ObjectOutputStream out) throws IOException
+     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
+     Теперь сериализация/десериализация пойдет по нашему сценарию :)
+     */
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        runner = new Thread(this);
+        runner.start();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
