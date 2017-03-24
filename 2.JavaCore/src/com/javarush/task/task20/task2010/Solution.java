ed7ab28a45aff043edcee22c5bf37c612686227a
Alexey Vedensky <avedensky@gmail.com>
2017-03-24 17:22:37 +0300

    task 2010 has done

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2010/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task20.task2010;
+
+import java.io.Serializable;
+
+/*
+Как сериализовать что-то свое?
+
+Сделайте так, чтобы сериализация класса Object была возможной.
+*/
+public class Solution {
+    public static class Object implements Serializable{
+        public String string1;
+        public String string2;
+    }
+
+    public static int countStrings;
+
+    public static class String implements Serializable{
+        private final int number;
+
+        public String() {
+            number = ++countStrings;
+        }
+
+        public void print() {
+            System.out.println("string #" + number);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
