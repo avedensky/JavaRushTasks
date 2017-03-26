740b7d6571e1d9f1d4f8c7507eefd559b39c6bf4
Alexey Vedensky <avedensky@gmail.com>
2017-03-27 00:14:06 +0300

    task0713 resolved

--- 1.JavaSyntax/src/com/javarush/task/task07/task0713/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0713/Solution.java
@@ -4,47 +4,55 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 /* 
 Играем в Jолушку
+
+Играем в Jолушку
+1. Введи с клавиатуры 20 чисел, сохрани их в список и рассортируй по трём другим спискам:
+Число нацело делится на 3 (x%3==0), нацело делится на 2 (x%2==0) и все остальные.
+Числа, которые делятся на 3 и на 2 одновременно, например 6, попадают в оба списка.
+2. Метод printList должен выводить на экран все элементы списка с новой строки.
+3. Используя метод printList выведи эти три списка на экран. Сначала тот, который для x%3, потом тот, который для x%2, потом последний.
 */
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
-        Scanner sc = new Scanner(System.in);
-
-        ArrayList<Integer> all = new ArrayList<Integer>();
-        ArrayList<Integer> listOther = new ArrayList<Integer>();
+        ArrayList<Integer> all = new ArrayList<>();
         ArrayList<Integer> list3 = new ArrayList<Integer>();
         ArrayList<Integer> list2 = new ArrayList<Integer>();
+        ArrayList<Integer> listOther = new ArrayList<Integer>();
 
-        int a=0;
-        for (int i=0;i<20;i++) {
-            a = Integer.parseInt(sc.nextLine());
-            all.add(a);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-            a = all.get (i);
-            if (a % 3 == 0 && a % 2 == 0) {
-                list2.add(a);
-                list3.add(a);
-                continue;
+        //Random rnd = new Random(100);
+        for (int i = 0; i < 20; i++)
+            all.add(Integer.parseInt(reader.readLine()));// = Integer.parseInt(reader.readLine());//rnd.nextInt(100);
+        reader.close();
+
+        for(int i = 0;i < 20;i++){
+            if(all.get(i)%3 == 0 && all.get(i)%2 ==0){
+                list3.add(all.get(i));
+                list2.add(all.get(i));
             }
-            if (a % 2 == 0) {
-                list2.add(a);
-                continue;
+            else if (all.get(i)%3 == 0){
+                list3.add(all.get(i));
             }
-            if (a % 3 == 0) {
-                list3.add(a);
-                continue;
+            else if(all.get(i)%2 == 0){
+                list2.add(all.get(i));
             }
-            listOther.add(a);
+            else {
+                listOther.add(all.get(i));
+            }
         }
 
         printList(list3);
         printList(list2);
         printList(listOther);
+
     }
 
     public static void printList(List<Integer> list) {
