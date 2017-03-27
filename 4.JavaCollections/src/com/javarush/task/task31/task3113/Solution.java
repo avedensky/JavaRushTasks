f0ec01820acf0ce62b0da9b6f9ec3003779a048b
Alexey Vedensky <avedensky@gmail.com>
2017-03-27 22:34:18 +0300

    Resolved Task3113

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -0,0 +1,66 @@
+package com.javarush.task.task31.task3113;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+/* 
+Что внутри папки?
+
+Напиши программу, которая будет считать подробную информацию о папке и выводить ее на консоль.
+
+Первым делом считай путь к папке с консоли.
+Если введенный путь не является директорией — выведи «[полный путь] — не папка» и заверши работу.
+Затем посчитай и выведи следующую информацию:
+
+Всего папок — [количество папок в директории]
+Всего файлов — [количество файлов в директории и поддиректориях]
+Общий размер — [общее количество байт, которое хранится в директории]
+
+Используй только классы и методы из пакета java.nio.
+*/
+public class Solution {
+    static long totalFolders = 0;
+    static long totalFiles = 0;
+    static long totalSize = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader conReader = new BufferedReader(new InputStreamReader(System.in));
+        String pathStr = conReader.readLine();
+        conReader.close();
+
+        Path pth = Paths.get(pathStr);
+
+        if (!Files.isDirectory(pth)) {
+            System.out.printf(pth.toAbsolutePath().toString() + " - не папка");
+            return;
+        }
+
+        Files.walkFileTree(pth, new Visitior());
+
+        System.out.println("Всего папок - " + (totalFolders-1));
+        System.out.println("Всего файлов - " + totalFiles);
+        System.out.println("Общий размер - " + totalSize);
+
+    }
+
+    private static class Visitior extends SimpleFileVisitor<Path> {
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            totalFolders += 1;
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            totalFiles += 1;
+            totalSize = totalSize + attrs.size();
+            return CONTINUE;
+        }
+    }
+}
