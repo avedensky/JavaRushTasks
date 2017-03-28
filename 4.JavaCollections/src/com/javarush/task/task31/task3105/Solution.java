bb55162080bc65a941518bd6df5ce9f14adf706d
Alexey Vedensky <avedensky@gmail.com>
2017-03-28 16:11:16 +0300

    Task 3105 has done

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
@@ -0,0 +1,69 @@
+package com.javarush.task.task31.task3105;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+/* 
+Добавление файла в архив
+
+В метод main приходит список аргументов.
+Первый аргумент — полный путь к файлу fileName.
+Второй аргумент — путь к zip-архиву.
+Добавить файл (fileName) внутрь архива в директорию ‘new‘.
+Если в архиве есть файл с таким именем, то заменить его.
+
+Пример входных данных:
+C:/result.mp3
+C:/pathToTest/test.zip
+
+Файлы внутри test.zip:
+a.txt
+b.txt
+
+После запуска Solution.main архив test.zip должен иметь такое содержимое:
+new/result.mp3
+a.txt
+b.txt
+
+Подсказка: нужно сначала куда-то сохранить содержимое всех энтри, а потом записать в архив все энтри вместе с добавленным файлом.
+Пользоваться файловой системой нельзя.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String fileName = args[0];
+        String zipFileName = args[1];
+        File file = new File(fileName);
+
+        Map<String, ByteArrayOutputStream> archivedFiles = new HashMap<>();
+        try (ZipInputStream zipReader = new ZipInputStream(new FileInputStream(zipFileName))) {
+            ZipEntry entry;
+            while ((entry = zipReader.getNextEntry()) != null {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int count = 0;
+                while ((count = zipReader.read(buffer)) != -1)
+                    byteArrayOutputStream.write(buffer, 0, count);
+
+                archivedFiles.put(entry.getName(), byteArrayOutputStream);
+            }
+        }
+
+        try (ZipOutputStream zipWriter = new ZipOutputStream(new FileOutputStream(zipFileName))) {
+            for (Map.Entry<String, ByteArrayOutputStream> pair : archivedFiles.entrySet()) {
+                if (pair.getKey().substring(pair.getKey().lastIndexOf("/") + 1).equals(file.getName())) continue;
+                zipWriter.putNextEntry(new ZipEntry(pair.getKey()));
+                zipWriter.write(pair.getValue().toByteArray());
+            }
+
+            ZipEntry zipEntry = new ZipEntry("new/" + file.getName());
+            zipWriter.putNextEntry(zipEntry);
+            Files.copy(file.toPath(), zipWriter);
+        }
+    }
+}
