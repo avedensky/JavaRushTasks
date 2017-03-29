fa9f5101e2e9a6cb42bf1968e9c0e5d41208caf7
Alexey Vedensky <avedensky@gmail.com>
2017-03-29 21:49:55 +0300

    Task 1925 has done

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task19.task1925;
+
+/* 
+Длинные слова
+
+В метод main первым параметром приходит имя файла1, вторым — файла2.
+Файл1 содержит слова, разделенные пробелом.
+Записать через запятую в Файл2 слова, длина которых строго больше 6.
+В конце файла2 запятой не должно быть.
+Закрыть потоки.
+
+Пример выходных данных в файл2:
+длинное,короткое,аббревиатура
+
+
+Требования:
+1. Программа НЕ должна считывать данные с консоли.
+2. Программа должна считывать содержимое первого файла (используй FileReader c конструктором String).
+3. Поток чтения из файла (FileReader) должен быть закрыт.
+4. Программа должна записывать через запятую во второй файл все слова из первого файла длина которых строго больше 6(используй FileWriter).
+5. Поток записи в файл (FileWriter) должен быть закрыт.
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String fileName1 = args[0];//"e:\9.txt"
+        String fileName2 = args[1];//"e:\10.txt"
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName1));
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileName2));
+        StringBuffer outText = new StringBuffer("");
+        while (fileReader.ready()) {
+            String line = fileReader.readLine();
+            String[] words = line.split(" ");
+            for (String word : words)
+                if (word.length() > 6)
+                    outText.append(word).append(",");
+        }
+
+        outText.delete(outText.length()-1,outText.length());
+        fileWriter.write(outText.toString());
+
+        System.out.println(outText.toString());
+        fileWriter.close();
+        fileReader.close();
+
+    }
+}
