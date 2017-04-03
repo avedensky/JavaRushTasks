6d286fd72bfabc322f92e641f09eae8ee2e5d212
Alexey Vedensky <avedensky@gmail.com>
2017-04-03 16:53:08 +0300

    Task 2213, 11 steps has done

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/FigureFactory.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * Created by Alexey Vedensky on 03.04.2017.
+ * MAIL: avedensky@gmail.com
+ */
+public class FigureFactory {
+    static Figure createRandomFigure(int x,int y) {
+        return null;//пока
+    }
+}
