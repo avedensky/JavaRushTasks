5c73aca14d83c914438c1d040261dc1437a9c30d
Alexey Vedensky <avedensky@gmail.com>
2017-04-03 16:14:02 +0300

    Task 2213, 1-6 steps has done

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -5,4 +5,33 @@ package com.javarush.task.task22.task2213;
  * MAIL: avedensky@gmail.com
  */
 public class Field {
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new int [height][width];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -1,29 +1,45 @@
 package com.javarush.task.task22.task2213;
 
-/*
-Тетрис(1)
-Давай напишем игру Тетрис!
-
-Наш Тетрис будет состоять из двух вещей: поля из клеток и фигурки, которая падает.
-Поэтому для начала создай три класса: Field(поле с клетками), Figure(фигура) и сам Tetris.
-
-Также нам понадобится метод main в классе Tetris.
-
-
-Требования:
-1. Класс Field должен быть создан в отдельном файле.
-2. Класс Figure должен быть создан в отдельном файле.
-3. Класс Tetris должен быть создан в отдельном файле.
-4. Класс Tetris должен содержать метод main.
- */
-
 
 /**
  * Created by Alexey Vedensky on 03.04.2017.
  * MAIL: avedensky@gmail.com
  */
 public class Tetris {
-    public static void main (String[] args) {
-        System.out.println("Start");
+    static Tetris game;
+
+    private Field field;
+    private Figure figure;
+
+    public void run() {
+
+    }
+
+    public void step() {
+
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Start Tetris");
+
+        game = new Tetris();
+        game.run();
     }
 }
