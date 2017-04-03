339d95643c7f01ce3bc74415a68d0b750ee595a7
Alexey Vedensky <avedensky@gmail.com>
2017-04-03 15:46:27 +0300

    Task 2213 has done

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * Created by Alexey Vedensky on 03.04.2017.
+ * MAIL: avedensky@gmail.com
+ */
+public class Field {
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task22.task2213;
+
+/*
+Тетрис(1)
+Давай напишем игру Тетрис!
+
+Наш Тетрис будет состоять из двух вещей: поля из клеток и фигурки, которая падает.
+Поэтому для начала создай три класса: Field(поле с клетками), Figure(фигура) и сам Tetris.
+
+Также нам понадобится метод main в классе Tetris.
+
+
+Требования:
+1. Класс Field должен быть создан в отдельном файле.
+2. Класс Figure должен быть создан в отдельном файле.
+3. Класс Tetris должен быть создан в отдельном файле.
+4. Класс Tetris должен содержать метод main.
+ */
+
+
+/**
+ * Created by Alexey Vedensky on 03.04.2017.
+ * MAIL: avedensky@gmail.com
+ */
+public class Tetris {
+    public static void main (String[] args) {
+        System.out.println("Start");
+    }
+}
