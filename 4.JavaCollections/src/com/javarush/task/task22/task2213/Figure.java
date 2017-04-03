8ab5280103f357959e5959e73225dab6111ececa
Alexey Vedensky <avedensky@gmail.com>
2017-04-03 16:41:07 +0300

    Task 2213, 8 steps has done

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -5,4 +5,37 @@ package com.javarush.task.task22.task2213;
  * MAIL: avedensky@gmail.com
  */
 public class Figure {
+    private int x;
+    private int y;
+    private int[][] matrix;
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+//    public void setX(int x) {
+//        this.x = x;
+//    }
+
+    public int getY() {
+        return y;
+    }
+
+//    public void setY(int y) {
+//        this.y = y;
+//    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+//    public void setMatrix(int[][] matrix) {
+//        this.matrix = matrix;
+//    }
 }
