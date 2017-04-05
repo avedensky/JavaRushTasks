97050e71f70e38dd0dccda1da0431a4ccc38e50f
Alexey Vedensky <avedensky@gmail.com>
2017-04-05 18:42:02 +0300

    Task 3303 has done

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3303/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task33.task3303;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+
+/* 
+Десериализация JSON объекта
+
+
+НЕОБХОДИМО: подключенные библиотеки Jackson Core, Bind и Annotation версии 2.6.1
+
+В метод convertFromJsonToNormal первым параметром приходит имя файла, который содержит один ДЖЕЙСОН объект.
+Вторым параметром приходит имя класса, объект которого находится в файле.
+Метод convertFromJsonToNormal должен вычитать объект из файла, преобразовать его из JSON и вернуть.
+
+
+Требования:
+1. В методе convertFromJsonToNormal должен быть создан объект типа ObjectMapper с помощью конструктора без параметров.
+2. Объект возвращаемый методом convertFromJsonToNormal должен быть получен с помощью метода readValue класса ObjectMapper.
+3. Метод convertFromJsonToNormal должен быть статическим.
+4. Метод convertFromJsonToNormal должен быть публичным.
+*/
+public class Solution {
+    public static <T> T convertFromJsonToNormal(String fileName, Class<T> clazz) throws IOException {
+        T result = null;
+        ObjectMapper mapper = new ObjectMapper();
+        StringReader reader;
+
+//        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+//            StringBuffer sb = new StringBuffer("");
+//            while (fileReader.ready())
+//                sb.append(fileReader.readLine());
+//
+//            reader = new StringReader(sb.toString());
+//        }
+
+        result = mapper.readValue(new FileReader(new File(fileName)), clazz);
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
