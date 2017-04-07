b61a39c7f1e1497f8d295f9c9fd7dc636a5bb8ae
Alexey Vedensky <avedensky@gmail.com>
2017-04-07 13:46:51 +0300

    Task 2515 6 has done

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -42,4 +42,25 @@ public abstract class BaseObject {
     public boolean isAlive() {
         return isAlive;
     }
+
+    public void draw () {
+    }
+
+    public void move () {
+
+    }
+
+    public void die () {
+        isAlive = false;
+    }
+
+    public boolean isIntersect(BaseObject o) {
+        //дистанция_между_объектами < max (радиус_первого_объекта, радиус_второго_объекта).
+
+        double deltaX = x - o.x;
+        double deltaY = y - o.y;
+        double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+        return distance <= Math.max (radius, o.getRadius());
+    }
 }
