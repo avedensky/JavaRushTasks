5b64995fd9f2154dd0c2d4207585e3006d251c16
Alexey Vedensky <avedensky@gmail.com>
2017-04-07 13:11:44 +0300

    Task 2515 3 has done

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -1,10 +1,55 @@
 package com.javarush.task.task25.task2515;
 
+import java.util.ArrayList;
+
 /**
  * Created by Alexey Vedensky on 07.04.2017.
  * MAIL: avedensky@gmail.com
  */
 public class Space {
+    private int width, height;
+    private SpaceShip ship;
+    private ArrayList<Ufo> ufos;
+    private ArrayList<Rocket> rockets;
+    private ArrayList<Bomb> bombs;
+
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.ufos = new ArrayList<>();
+        this.rockets = new ArrayList<>();
+        this.bombs = new ArrayList<>();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public SpaceShip getShip() {
+        return ship;
+    }
+
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
+    }
+
+    public ArrayList<Ufo> getUfos() {
+        return ufos;
+    }
+
+    public ArrayList<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public ArrayList<Bomb> getBombs() {
+        return bombs;
+    }
+
     public static void main(String[] args) {
 
     }
