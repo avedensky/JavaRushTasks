585218b82ccaeb56bbbf5f6aa2c4e25ed1fb9e0d
Alexey Vedensky <avedensky@gmail.com>
2017-04-07 16:17:14 +0300

    Task 2515 8 has done

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -5,7 +5,7 @@ package com.javarush.task.task25.task2515;
  * MAIL: avedensky@gmail.com
  */
 public class Canvas {
-    private int width,height;
+    private int width, height;
     private char[][] matrix;
 
     public Canvas(int width, int height) {
@@ -24,5 +24,33 @@ public class Canvas {
 
     public char[][] getMatrix() {
         return matrix;
+    }
+
+    public void setPoint(double x, double y, char c) {
+        if (x < 0 || y < 0 || x >= matrix[0].length || y >= matrix.length)
+            return;
+
+
+        matrix[(int) Math.round(y)][(int) Math.round(x)] = c;
+    }
+
+
+    /*
+Второй метод — drawMatrix копирует переданную ему картинку (матрицу) в матрицу Canvas.
+И не просто копирует, а начиная с координат x, y.
+
+В методе надо:
+а) с помощью двух вложенных циклов пройтись по всем ячейкам переданной картинки,
+б) если значение ячейки matrix[i][j] не равно 0, то покрасить в матрице объекта Canvas точку (x+j, y+i) в цвет c:
+setPoint(x+j, y+i, c)
+     */
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0)
+                    setPoint(x + j, y + i, c);
+            }
+        }
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -5,21 +5,6 @@ package com.javarush.task.task25.task2515;
  * MAIL: avedensky@gmail.com
  */
 
-/*Давай напишем новую компьютерную игрушку.
-У нас будет космический корабль, который стреляет ракетами в НЛО.
-А НЛО в свою очередь сбрасывает на корабль бомбы.
-Ну и, конечно, всё дело происходит в космосе.
-
-Нам понадобятся классы: Space(космос), SpaceShip(космический корабль) и Ufo(НЛО).
-Создай классы: Space, SpaceShip, Ufo.
-Добавь метод main в класс Space.
-
-
-Требования:
-1. Создай в отдельном файле класс Space.
-2. Создай в отдельном файле класс SpaceShip.
-3. Создай в отдельном файле класс Ufo.
-4. Добавь метод main в класс Space.*/
 public class Ufo extends BaseObject{
     public Ufo(double x, double y, double radius) {
         super(x, y, radius);
