477ae2f98af8065004bc0cbf7151538870d1d41d
Alexey Vedensky <avedensky@gmail.com>
2017-04-14 17:16:52 +0300

    Task 3310 (1) has done)

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by Alexey Vedensky on 14.04.2017.
+ * MAIL: avedensky@gmail.com
+ */
+public class Shortener {
+    public Long getId(String string) {
+
+        return null;
+    }
+
+    public String getString(Long id) {
+        return null;
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by Alexey Vedensky on 14.04.2017.
+ * MAIL: avedensky@gmail.com
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
