b00ad69fbdd0b6dfc8cd44f7795154ff4bd4b3a5
Alexey Vedensky <avedensky@gmail.com>
2017-04-15 20:45:32 +0300

    Task 3310 has done

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/Entry.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task33.task3310.strategy;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Created by Alexey Vedensky on 15.04.2017.
+ * MAIL: avedensky@gmail.com
+ */
+public class Entry implements Serializable {
+    Long key;
+    String value;
+    Entry next;
+    int hash;
+
+    public Entry(int hash, Long key, String value, Entry next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+        this.hash = hash;
+    }
+
+    public Long getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return (key   == null ? 0 :   key.hashCode()) ^ (value == null ? 0 : value.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Entry))
+            return false;
+
+        Long k1 = getKey();
+        Long k2 = ((Entry) o).getKey();
+        String v1 = getValue();
+        String v2 = ((Entry) o).getValue();
+        if (k1 == k2 || (k1 != null && k1.equals(k2))) {
+            if (v1 == v2 || (v1 != null && v1.equals(v2))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    @Override
+    public String toString() {
+        return new String (key + "=" + value);
+    }
+}
