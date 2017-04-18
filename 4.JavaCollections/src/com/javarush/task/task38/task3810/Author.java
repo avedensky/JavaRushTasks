eeac202214c35dc67b7ac92c4b5f1198f8b8a1da
Alexey Vedensky <avedensky@gmail.com>
2017-04-18 19:24:38 +0300

    Task 3810 has update and done

--- 4.JavaCollections/src/com/javarush/task/task38/task3810/Author.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3810/Author.java
@@ -1,7 +1,9 @@
 package com.javarush.task.task38.task3810;
 
+
+
 public @interface Author {
     //напиши свой код
-    String value();
-    Position position() default Position.JUNIOR;
+    String value() default "";
+    Position position() default Position.OTHER;
 }
