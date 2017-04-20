a0bcf2b6a506cd1bb586a34faa01184c63eac1b0
Alexey Vedensky <avedensky@gmail.com>
2017-04-20 23:10:43 +0300

    Task 3707 has done

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -0,0 +1,84 @@
+package com.javarush.task.task37.task3707;
+
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * Created by Alexey on 20.04.2017.
+ */
+
+public class AmigoSet<E> extends AbstractSet<E> implements Cloneable, Serializable, Set<E>  {
+
+    public AmigoSet() {
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
