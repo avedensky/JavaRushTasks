d24b5e723566a2ead57f076f0ed036420dabcdd3
Alexey Vedensky <avedensky@gmail.com>
2017-05-08 19:08:42 +0300

    Task 2104 has done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Equals and HashCode
+
+В классе Solution исправить пару методов equals/hashCode в соответствии с правилами реализации этих методов
+(детали уточни у своего любимого поисковика).
+Обе строки first и last должны принимать участие в сравнении с помощью метода equals и вычислении hashcode.
+Метод main не участвует в тестировании.
+
+
+Требования:
+1. Хешкоды одинаковых объектов должны быть равны.
+2. Метод equals должен проверять равен ли переданный объект текущему(сравнение через ==).
+3. Метод equals должен проверять является ли переданный объект объектом класса Solution.
+4. Метод equals должен возвращать true в случае, если поля first и last равны у переданного объекта и текущего(не забудь что они могут быть равны null).
+5. Должно быть обеспечено корректное поведение HashSet с типом элементов Solution.
+*/
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+    //Метод equals должен проверять является ли переданный объект объектом класса Solution
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == null)
+            return false;
+
+        if (!(o instanceof Solution)){
+            return false;
+        }
+
+        if (this == o) return true;
+
+        Solution solution = (Solution) o;
+
+        if (first != null ? !first.equals(solution.first) : solution.first != null) return false;
+        if (last != null ? !last.equals(solution.last) : solution.last != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}
