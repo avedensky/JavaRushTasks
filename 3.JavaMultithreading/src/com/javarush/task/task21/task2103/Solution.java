1b357b5f9f1e61d0d09d97c20c96fbfec7510847
Alexey Vedensky <avedensky@gmail.com>
2017-05-08 18:26:30 +0300

    Task 2103 has done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task21.task2103;
+
+/* 
+Все гениальное - просто!
+*/
+public class Solution {
+    public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculate (true,true,true, true));
+        System.out.println(calculate (true,true,true, false));
+
+        System.out.println(calculate (true,true,false, false));
+        System.out.println(calculate (true,true,false, true));
+
+        System.out.println(calculate (true,false,false, true));
+        System.out.println(calculate (true,false,true, true));
+        System.out.println(calculate (false,false,false, true));
+        System.out.println(calculate (true,false,true, false));
+
+        System.out.println(calculate (false,true,false, true));
+        System.out.println(calculate (true,false,true, true));
+    }
+}
