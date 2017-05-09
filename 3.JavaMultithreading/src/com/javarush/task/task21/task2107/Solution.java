ff5a13986f80c2fa611a7f61de0d19c2dd0fe9a4
Alexey Vedensky <avedensky@gmail.com>
2017-05-09 15:42:08 +0300

    Task 2107 has done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
@@ -0,0 +1,85 @@
+package com.javarush.task.task21.task2107;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/* 
+Глубокое клонирование карты
+
+Обеспечь возможность клонирования объекта класса Solution используя глубокое клонирование.
+Данные в карте users также должны быть клонированы.
+Не забудь о методах equals и hashCode для корректного добавления элементов типа User в HashMap.
+
+
+Требования:
+1. Класс Solution должен поддерживать интерфейс Cloneable.
+2. Класс User должен поддерживать интерфейс Cloneable.
+3. В классе User должен быть корректно реализован метод clone.
+4. В классе Solution должен быть корректно реализован метод clone.
+
+*/
+public class Solution implements Cloneable {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.users.put("Hubert", new User(172, "Hubert"));
+        solution.users.put("Zapp", new User(41, "Zapp"));
+        Solution clone = null;
+        try {
+            clone = solution.clone();
+            System.out.println(solution);
+            System.out.println(clone);
+
+            System.out.println(solution.users);
+            System.out.println(clone.users);
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    protected Map<String, User> users = new LinkedHashMap();
+
+    public static class User implements Cloneable {
+        int age;
+        String name;
+
+        public User(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        protected User clone() throws CloneNotSupportedException {
+            return new User(this.age, this.name);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof User)) return false;
+
+            User user = (User) o;
+
+            if (age != user.age) return false;
+            return name.equals(user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = age;
+            result = 31 * result + name.hashCode();
+            return result;
+        }
+    }
+
+    @Override
+    protected Solution clone() throws CloneNotSupportedException {
+        Solution solution = new Solution();
+        for (Map.Entry<String, User> pair : this.users.entrySet()) {
+            User user = pair.getValue().clone();
+            solution.users.put(pair.getKey(), user);
+        }
+        return solution;
+    }
+}
