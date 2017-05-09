97f64a03fe1707fadc63aa496a75bbefc875f0fe
Alexey Vedensky <avedensky@gmail.com>
2017-05-09 17:31:43 +0300

    Task 2112 has done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2112/FakeConnection.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task21.task2112;
+
+public class FakeConnection implements AutoCloseable{
+
+    public FakeConnection() {
+        System.out.println("Creating database connection...");
+    }
+
+    public void unsupportedOperation() {
+        System.out.println("Operation is not supported yet!");
+        throw new RuntimeException("UnsupportedOperation!");
+    }
+
+    public void usefulOperation() {
+        System.out.println("Executing useful operation.");
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Closing database connection...");
+    }
+}
