cbae85c30bce67262943521b92bf515063011ed4
Alexey Vedensky <avedensky@gmail.com>
2017-05-09 15:50:52 +0300

    Task 2108 has done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2108/Solution.java
@@ -0,0 +1,67 @@
+package com.javarush.task.task21.task2108;
+
+/* 
+Клонирование растений
+
+Класс Plant не должен реализовывать интерфейс Cloneable
+Реализуй механизм глубокого клонирования для Tree.
+
+
+Требования:
+1. Класс Plant не должен поддерживать интерфейс Cloneable.
+2. Класс Tree должен поддерживать интерфейс Cloneable.
+3. Класс Tree должен быть потомком класса Plant.
+4. В классе Tree должен быть корректно реализован метод clone.
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Tree tree = new Tree("willow", new String[]{"s1", "s2", "s3", "s4"});
+        Tree clone = null;
+        try {
+            clone = tree.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(tree);
+        System.out.println(clone);
+
+        System.out.println(tree.branches);
+        System.out.println(clone.branches);
+    }
+
+    public static class Plant{
+        private String name;
+
+        public Plant(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static class Tree extends Plant implements Cloneable {
+        private String[] branches;
+
+        public Tree(String name, String[] branches) {
+            super(name);
+            this.branches = branches;
+        }
+
+        public String[] getBranches() {
+            return branches;
+        }
+
+        @Override
+        protected Tree clone() throws CloneNotSupportedException {
+            String[] strings = new String[branches.length];
+            for (int i=0;i<branches.length;i++) {
+                strings[i] = branches[i];
+            }
+
+            return new Tree(super.getName(), strings);
+        }
+    }
+}
