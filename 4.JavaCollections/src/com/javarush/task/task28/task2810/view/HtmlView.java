9bdc219e2140c3d49b8372da65394b920db44ab2
Alexey Vedensky <avedensky@gmail.com>
2017-05-10 14:34:20 +0300

    Task 2810 (14) has done

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -4,11 +4,20 @@ import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 /**
  * Created by Alexey on 10.05.2017.
  */
+
+
+/*1. В классе HtmlView в методе updateFile открой поток для записи в файл.
+        2. Запиши в файл данные, которые метод updateFile получает аргументом.
+        3. Закрой поток записи в файл.*/
 
 public class HtmlView implements View {
     private Controller controller;
@@ -24,6 +33,15 @@ public class HtmlView implements View {
     }
 
     private void updateFile(String s) {
+        try {
+            OutputStream outputStream = new FileOutputStream(filePath);
+            outputStream.write(s.getBytes());
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
