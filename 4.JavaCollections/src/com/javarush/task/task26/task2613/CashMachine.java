1a41f66add23f5581b8821c844a2eee437af93a1
Alexey Vedensky <avedensky@gmail.com>
2017-05-11 13:28:01 +0300

    Task 2613 (1) has done

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task26.task2613;
+
+/**
+ * Created by Alexey on 11.05.2017.
+ */
+
+enum Operation {INFO, DEPOSIT, WITHDRAW, EXIT}
+
+public class CashMachine {
+    public static void main(String[] args) {
+
+    }
+}
