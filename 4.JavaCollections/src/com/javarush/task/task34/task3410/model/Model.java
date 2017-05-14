7a84a5f23a83e96257fbd6d8162e82f2089cc3db
Alexey Vedensky <avedensky@gmail.com>
2017-05-14 16:11:41 +0300

    Task 3410 (11) has done

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -1,15 +1,38 @@
 package com.javarush.task.task34.task3410.model;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+
+import java.nio.file.Paths;
+
 /**
  * Created by Alexey on 14.05.2017.
  */
 public class Model {
     public static int FIELD_CELL_SIZE = 20;
     private EventListener eventListener;
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+    private LevelLoader levelLoader  = new LevelLoader(Paths.get("..\\res\\levels.txt")); ;
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
+    }
+
+    public void restartLevel(int level) {
+        this.gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart() {
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel = currentLevel + 1;
+        restartLevel(currentLevel);
     }
 
 }
