c90ed7a3eb8661eb7f189d075aedafd3ab4e3ea1
Alexey Vedensky <avedensky@gmail.com>
2017-05-14 17:46:25 +0300

    Task 2312 has done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task23.task2312;
+
+/**
+ * Created by Alex Vedensky on 14.05.2017.
+ * MAIL: avedensky@gmail.com
+ */
+public class Room {
+
+    public static void main(String[] args) {
+
+    }
+}
