9fe2779e2a11f1b0a81afb4fd3781acbc7ae1fb9
Alexey Vedensky <avedensky@gmail.com>
2017-05-14 17:49:59 +0300

    Task 2312 (4) has done

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
@@ -6,4 +6,25 @@ package com.javarush.task.task23.task2312;
  */
 public class Mouse {
     private int x, y;
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
