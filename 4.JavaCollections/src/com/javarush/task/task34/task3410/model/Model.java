ac1058a77d3ebf80872140584acca37cc937e1a7
Alexey Vedensky <avedensky@gmail.com>
2017-05-14 17:15:20 +0300

    Task 3410 (15) has done

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -12,7 +12,8 @@ public class Model {
     private EventListener eventListener;
     private GameObjects gameObjects;
     private int currentLevel = 1;
-    private LevelLoader levelLoader  = new LevelLoader(Paths.get("..\\res\\levels.txt")); ;
+    private LevelLoader levelLoader = new LevelLoader(Paths.get("..\\res\\levels.txt"));
+    ;
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
@@ -35,8 +36,97 @@ public class Model {
         restartLevel(currentLevel);
     }
 
-    public void move (Direction direction) {
+    public void move(Direction direction) {
+        Player player = gameObjects.getPlayer();
 
+        if (checkWallCollision(player, direction)) {
+            return;
+        }
+        if (checkBoxCollisionAndMoveIfAvaliable(direction)) {
+            return;
+        }
+
+        switch (direction) {
+            case LEFT:
+                player.move(-FIELD_CELL_SIZE, 0);
+                break;
+            case RIGHT:
+                player.move(FIELD_CELL_SIZE, 0);
+                break;
+            case UP:
+                player.move(0, -FIELD_CELL_SIZE);
+                break;
+            case DOWN:
+                player.move(0, FIELD_CELL_SIZE);
+        }
+        checkCompletion();
+    }
+
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction) {
+        for (Wall wall : gameObjects.getWalls()) {
+            if (gameObject.isCollision(wall, direction))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean checkBoxCollisionAndMoveIfAvaliable(Direction direction) {
+        Player player = gameObjects.getPlayer();
+        GameObject stoped = null;
+        for (GameObject gameObject : gameObjects.getAll()) {
+            if (!(gameObject instanceof Player) && !(gameObject instanceof Home) && player.isCollision(gameObject, direction)) {
+                stoped = gameObject;
+            }
+        }
+
+        if ((stoped == null)) {
+            return false;
+        }
+
+        if (stoped instanceof Box) {
+            Box stopedBox = (Box) stoped;
+            if (checkWallCollision(stopedBox, direction)) {
+                return true;
+            }
+            for (Box box : gameObjects.getBoxes()) {
+                if (stopedBox.isCollision(box, direction)) {
+                    return true;
+                }
+            }
+
+            switch (direction) {
+                case LEFT:
+                    stopedBox.move(-FIELD_CELL_SIZE, 0);
+                    break;
+                case RIGHT:
+                    stopedBox.move(FIELD_CELL_SIZE, 0);
+                    break;
+                case UP:
+                    stopedBox.move(0, -FIELD_CELL_SIZE);
+                    break;
+                case DOWN:
+                    stopedBox.move(0, FIELD_CELL_SIZE);
+            }
+        }
+        return false;
+
+    }
+
+    public void checkCompletion() {
+        boolean isLevelCompleted = true;
+
+        for (Home home : gameObjects.getHomes()) {
+            boolean chk = false;
+
+            for (Box box : gameObjects.getBoxes()) {
+                if ((box.getX() == home.getX()) && (box.getY() == home.getY()))
+                    chk = true;
+            }
+            if (!chk) isLevelCompleted = false;
+        }
+
+        if (isLevelCompleted)
+            eventListener.levelCompleted(currentLevel);
     }
 
 }
