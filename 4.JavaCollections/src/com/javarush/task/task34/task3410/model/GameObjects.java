6eeba83dc977c22f51ffcdfde74e4baaa78ca0d7
Alexey Vedensky <avedensky@gmail.com>
2017-05-14 15:44:29 +0300

    Task 3410 (8) has done

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/GameObjects.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task34.task3410.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Alex Vedensky on 14.05.2017.
+ * MAIL: avedensky@gmail.com
+ */
+public class GameObjects {
+    private Set<Wall> walls;
+    private Set<Box> boxes;
+    private Set<Home> homes;
+    private Player player;
+
+    public GameObjects(Set<Wall> walls, Set<Box> boxes, Set<Home> homes, Player player) {
+        this.walls = walls;
+        this.boxes = boxes;
+        this.homes = homes;
+        this.player = player;
+    }
+
+    public Set<GameObject> getAll() {
+        Set<GameObject> result = new HashSet<>();
+        result.addAll(getWalls());
+        result.addAll(getBoxes());
+        result.addAll(getHomes());
+        result.add(getPlayer());
+        return result;
+    }
+
+    public Set<Wall> getWalls() {
+        return walls;
+    }
+
+    public void setWalls(Set<Wall> walls) {
+        this.walls = walls;
+    }
+
+    public Set<Box> getBoxes() {
+        return boxes;
+    }
+
+    public void setBoxes(Set<Box> boxes) {
+        this.boxes = boxes;
+    }
+
+    public Set<Home> getHomes() {
+        return homes;
+    }
+
+    public void setHomes(Set<Home> homes) {
+        this.homes = homes;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
