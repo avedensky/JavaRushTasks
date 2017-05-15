94e826e987e71bde2f73102b262bb201e2b201d8
Alexey Vedensky <avedensky@gmail.com>
2017-05-15 20:19:24 +0300

    Task 2312 has done

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeDirection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeDirection.java
@@ -4,5 +4,4 @@ package com.javarush.task.task23.task2312;
  * Created by Alex Vedensky on 14.05.2017.
  * MAIL: avedensky@gmail.com
  */
-public enum SnakeDirection {
-}
+public enum SnakeDirection { UP, DOWN, LEFT, RIGHT}
