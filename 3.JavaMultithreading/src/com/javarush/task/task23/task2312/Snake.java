bad3f706353b59b85d773afeb2a89f2212e56629
Alexey Vedensky <avedensky@gmail.com>
2017-05-16 17:43:21 +0300

    Task 2312 (7) has done

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,8 +1,30 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.List;
+
 /**
  * Created by Alex Vedensky on 14.05.2017.
  * MAIL: avedensky@gmail.com
  */
 public class Snake {
+    private List<SnakeSection> sections;
+
+    private boolean isAlive;
+    private SnakeDirection direction;
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
 }
