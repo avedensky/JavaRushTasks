a7594da32d6b022bfa26f31fc6217eb675813d11
Alexey Vedensky <avedensky@gmail.com>
2017-05-16 17:29:23 +0300

    Task 2312 (6) has done

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -5,4 +5,26 @@ package com.javarush.task.task23.task2312;
  * MAIL: avedensky@gmail.com
  */
 public class SnakeSection {
+    private int x,y;
+
+    public SnakeSection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
