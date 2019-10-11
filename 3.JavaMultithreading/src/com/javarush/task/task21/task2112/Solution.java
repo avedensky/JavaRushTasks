4fb8d7d18b40c6d0d8042e194b985a8ac7703325
korea <e.kortov@bssys.com>
2019-10-11 19:20:29 +0400

    Туда же в придачу

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2112/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2112/Solution.java
@@ -32,7 +32,7 @@ public class Solution {
     public static void main(String[] args) {
         DBConnectionManager dbConnectionManager = new DBConnectionManager();
         try (FakeConnection fakeConnection = dbConnectionManager.getFakeConnection()) {
-            System.out.println("Entering the body of try block.");
+            System.out.println("Entering body of try block.");
             fakeConnection.usefulOperation();
             fakeConnection.unsupportedOperation();
         } catch (Exception e) {
